d9489811d38f99b2de7873fe060c6ff13d678095
Sudha Redroutu <156336019+Sudha-Redrouthu@users.noreply.github.com>
2025-07-10 18:00:35 -0400

    Create variables.tf
    Signed-off-by: Sudha Redroutu <156336019+Sudha-Redrouthu@users.noreply.github.com>

---
+++ terraform/modules/aks/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  type = string
+}
+
+variable "dns_prefix" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
