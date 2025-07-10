b8421bec314e2557420770f65e5c56e348f40f32
Sudha Redroutu <156336019+Sudha-Redrouthu@users.noreply.github.com>
2025-07-10 18:23:23 -0400

    Update main.tf
    Signed-off-by: Sudha Redroutu <156336019+Sudha-Redrouthu@users.noreply.github.com>

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -5,23 +5,20 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.dns_prefix
 
   default_node_pool {
-   name       = "default"
-   node_count = 2
-   vm_size    = "Standard_B2s"   # ✅ Change from Standard_DS2_v2 to Standard_B2s
-   vnet_subnet_id = var.subnet_id
-   }
-
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_B2s"      # Updated to a supported VM size
+    vnet_subnet_id  = var.subnet_id
+  }
 
   identity {
     type = "SystemAssigned"
   }
 
- network_profile {
-  network_plugin     = "azure"
-  service_cidr       = "10.1.0.0/16"        # ✅ Must NOT overlap with VNet/Subnet
-  dns_service_ip     = "10.1.0.10"          # ✅ Must be within service_cidr
-  docker_bridge_cidr = "172.17.0.1/16"
- }
-
-
+  network_profile {
+    network_plugin     = "azure"
+    service_cidr       = "10.1.0.0/16"      # Avoids conflict with VNet CIDR
+    dns_service_ip     = "10.1.0.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+  }
 }
